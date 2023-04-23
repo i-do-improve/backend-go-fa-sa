d007be525471a49380a85ce3c3be7cd2cbb937b9
sobaman <sobaman522@gmail.com>
2023-04-24 00:01:53 +0900

    [2023-04-23] 스프링 이니셜라이저 초기 세팅

---
+++ back/src/test/java/gopasa/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package gopasa.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
