28b2729bbfb39dca52cc21c16d8dc8b363cf6bcc
ezz <ezz@59.5.206.244>
2023-05-01 17:48:12 +0900

    초기설정 수정

---
+++ backend-gopasa/src/main/java/Domo/BackendGopasaApplication.java
@@ -0,0 +1,13 @@
+package Domo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGopasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGopasaApplication.class, args);
+	}
+
+}
---
+++ backend-gopasa/src/test/java/Domo/BackendGopasaApplicationTests.java
@@ -0,0 +1,13 @@
+package Domo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGopasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
