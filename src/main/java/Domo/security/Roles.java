7afa3498ea899d0223125ffc79e177d12a1fb72f
ezz <ezz@59.5.206.244>
2023-05-02 21:25:06 +0900

    Roles 설명추가

--- src/main/java/Domo/security/Roles.java
+++ src/main/java/Domo/security/Roles.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor //파이널 필드를 파라미터로 구성하는 생성자
 public enum Roles {
 
-	USER("ROLE_USER"), //0
-	ADMIN("ROLE_ADMIN"), //1
-	BASIC("ROLE_BASIC"),
-	PREMIUM("ROLE_PREMIUM");
+	USER("ROLE_USER"), //기본 유저 권한
+	ADMIN("ROLE_ADMIN"), //관리자 권한
+	BASIC("ROLE_BASIC"), //베이직 구독 권한
+	PREMIUM("ROLE_PREMIUM"); //프리미엄 구독 권한
 	
 	
 	private final String role; //getRole() //ROLE_USER or ROLE_ADMIN
