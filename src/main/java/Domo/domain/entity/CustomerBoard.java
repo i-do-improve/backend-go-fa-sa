34e36906bf36739f0383c87666fb62027fa49e7d
sobaman <sobaman522@gmail.com>
2023-06-04 18:38:11 +0900

    [2023-06-04] CustomerBoard 올립니다 + memId -> member_id

--- src/main/java/Domo/domain/entity/CustomerBoard.java
+++ src/main/java/Domo/domain/entity/CustomerBoard.java
@@ -33,7 +33,7 @@ public class CustomerBoard extends BaseDateEntity{
 	
 	@JoinColumn
     @ManyToOne(cascade = CascadeType.DETACH)
-    private MemberEntity member_Id; //작성자 회원 번호
+    private MemberEntity member_id; //작성자 회원 번호
 	
 	
 }
