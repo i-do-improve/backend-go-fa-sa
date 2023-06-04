a27baf7fd4b649a5a0eccb52e0048648ab6ade9d
sobaman <122892573+sobaman@users.noreply.github.com>
2023-06-04 18:30:42 +0900

    [2023-06-04] 댓글 엔티티 올립니다. (#53)
    * [2023-06-04]댓글 엔티티 올립니다.
    
    * [2023-06-04] 댓글 엔티티 올립니다 + @ManyToOne수정1

---
+++ src/main/java/Domo/domain/entity/Reply.java
@@ -0,0 +1,42 @@
+package Domo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@DynamicUpdate
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "reply")
+@Entity
+public class Reply extends BaseDateEntity{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long key; // 댓글 번호
+
+    @Column(nullable = false)
+    private String content;    // 댓글 내용
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.DETACH)
+    private MemberEntity Id; // 게시글 번호
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.DETACH)
+    private MemberEntity member_Id; // 작성자 회원 번호
+
+
+
+
+
+
+
+
+}
