cee3278a4a1813af2064e892202ab08cb1ea9d3f
sobaman <122892573+sobaman@users.noreply.github.com>
2023-06-08 20:07:19 +0900

    [2023-06-08] review 엔티티 입니다 (#58)

---
+++ src/main/java/Domo/domain/entity/ReviewEntity.java
@@ -0,0 +1,36 @@
+package Domo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@DynamicUpdate
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "review")
+@Entity
+public class ReviewEntity extends BaseDateEntity{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;   // 리뷰번호
+
+    @Column(nullable = false)
+    private String content; // 리뷰내용
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.DETACH)
+    private MemberEntity member_id;
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.DETACH)
+    private CatItemEntity cat_item_id;
+
+
+}
