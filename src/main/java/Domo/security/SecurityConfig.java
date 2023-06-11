5d3c6160998bb585e5bedf0d4d809a16ef395653
ezzange <107745857+ezzange@users.noreply.github.com>
2023-06-11 14:35:21 +0900

    모든페이지 보안 임시 공개, 로그인 성공시 메인화면으로 이동추가

--- src/main/java/Domo/security/SecurityConfig.java
+++ src/main/java/Domo/security/SecurityConfig.java
@@ -1,13 +1,21 @@
 package Domo.security;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
 
@@ -36,18 +44,27 @@ public class SecurityConfig {
 		http
 			.authorizeRequests(authorize -> authorize
 					//.antMatchers("/css/**").permitAll()					
-					.antMatchers("/","/admin").permitAll()
-					//.antMatchers("/").permitAll()
+					.antMatchers("/**").permitAll()
 					//.antMatchers("/admin/**").hasAnyRole("ADMIN")
 				.anyRequest().authenticated()
 			)
-//			.formLogin(formLogin->formLogin
-//					.loginPage("/member/login")
-//					.loginProcessingUrl("/member/login")//form태그의 action
-//					.permitAll()
-//					)
-//			.csrf(csrf->csrf.disable())		
-			;
+			.formLogin(form->form
+                    .loginPage("/")             //[GET]
+                    .loginProcessingUrl("/signin")      //[POST] form태그의 action
+					.defaultSuccessUrl("/member/main")
+                    .successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            response.sendRedirect("/member/main");
+                        }
+                    })
+                    .usernameParameter("email")         //username -> email
+                    .passwordParameter("pass")          //password -> pass
+                    .permitAll()
+
+            )
+            .csrf(csrf->csrf.disable())
+    ;
 		return http.build();
 	}
 }
